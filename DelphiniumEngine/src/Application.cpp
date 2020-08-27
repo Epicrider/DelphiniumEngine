6a72411847ba86ae3e68e53b74e3cce8c42d6137
Epicrider <omarepic1@gmail.com>
2020-08-27 10:46:30 -0700

    Add Vertex and Fragment Shaders

--- DelphiniumEngine/src/Application.cpp
+++ DelphiniumEngine/src/Application.cpp
@@ -4,6 +4,59 @@
 
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str(); // or &source[0] since the beginning of the str
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " << 
+			(type == GL_VERTEX_SHADER ? "vertex" : "fragment") 
+			<< " shader!" << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;	
+	}
+
+	return id;
+}
+
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+
+	// The Strings are meant to be the actual source code
+	// We usually read from text files
+
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned  int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	// We got two different files and now we need to compile and link them
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	// Delete the object files when dealing with Microsoft's compiler. We already have the program stored within the program
+	// We don't want to detach shaders because that would delete the shader source, and since it takes a trivial amount of memory
+	// Might also be helpful for debugging later
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+	
+	return program;
+}
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -66,6 +119,30 @@ int main(void)
 	// and 0 is the distance between two positions in a series of vertexes (because there could be color or something in between)
 	// in the same vertex, so we must know how much data is taken up between the two positions
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); // One Attribute in a Vertex (and at the moment, we only have one)
+
+	std::string vertexShader = 
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = position;\n"
+		"}\n";
+
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+		"}\n"; 
+
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
+
 
 	// We want stuff that is changed in this loop, but if it is not changed
 	// We would want to keep it in a buffer and leave it alone
