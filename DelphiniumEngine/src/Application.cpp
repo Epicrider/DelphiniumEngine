e99a30b92a7c2700cc988a6710d8deae922e2c8f
Epicrider <omarepic1@gmail.com>
2020-08-31 19:07:03 -0700

    Add External File Shader Reading Capability
    Added function to read a file .shader file and detect what shader it is,
    as well as added a local strings that will contain the src that is
    gathered in the function.

--- DelphiniumEngine/src/Application.cpp
+++ DelphiniumEngine/src/Application.cpp
@@ -3,13 +3,54 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+
+	std::ifstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+				type = ShaderType::VERTEX;
+			else if (line.find("fragment") != std::string::npos)
+				type = ShaderType::FRAGMENT;
+		}
+		else
+		{
+			ss[(int)type] << line << '\n';
+		}
+	}
+	
+	return { ss[0].str(), ss[1].str() };
+
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
 
 	unsigned int id = glCreateShader(type);
 	const char* src = source.c_str(); // or &source[0] since the beginning of the str
-	glShaderSource(id, 1, &src, nullptr);
+	glShaderSource(id, 1, &src, nullptr); // Strings not parsed or parsed, just copied
 	glCompileShader(id);
 
 	int result;
@@ -102,8 +143,9 @@ int main(void)
 						// buffer id for an object and it refers to a specific
 						// shader, and you call that id when you need to buffer something
 						// Can you buffer # pls
+
 	// Selecting which buffer to use to render your triangle
-	glGenBuffers(1, &buffer);
+	glGenBuffers(1, &buffer); // Making 1 buffer, and putting name in usigned int buffer
 	glBindBuffer(GL_ARRAY_BUFFER, buffer); // Kind of like selecting a layer in a Digital Art Program
 	// Now we need to provide the buffer with data
 	// Can do it later or now since we know what the data is now
@@ -120,27 +162,15 @@ int main(void)
 	// in the same vertex, so we must know how much data is taken up between the two positions
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); // One Attribute in a Vertex (and at the moment, we only have one)
 
-	std::string vertexShader = 
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n"
-		"}\n";
 
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 color;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-		"}\n"; 
+	ShaderProgramSource source = ParseShader("Res/Shaders/Basic.shader");
+	std::cout << "VERTEX" << std::endl;
+	std::cout << source.VertexSource << std::endl;
+	std::cout << "FRAGMENT" << std::endl;
+	std::cout << source.FragmentSource << std::endl;
 
-	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
 
 
@@ -169,6 +199,8 @@ int main(void)
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+
+	glDeleteProgram(shader);
 
 	glfwTerminate();
 	return 0;
